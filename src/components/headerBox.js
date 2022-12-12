1035aefe3ee77b90a8d5fdf57563a61b9c425378
Joshua Grossman <josh1794@gmail.com>
2022-12-12 12:08:23 -0500

    Working on new header section

---
+++ src/components/headerBox.js
@@ -0,0 +1,39 @@
+import styles from '../../styles/Home.module.scss';
+
+export default function HeaderBox() {
+  return (
+    <div className={styles.headerBox}>
+      <h1 className={styles.titleText}>Joshua Grossman</h1>
+      <div className={styles.social}>
+        <a
+          className={styles.sociala}
+          aria-label='Linkedin Profile'
+          href='https://www.linkedin.com/in/joshua-grossman17/'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <i className='fab fa-linkedin-in'></i>
+        </a>
+        <a
+          className={styles.sociala}
+          aria-label='Github Profile'
+          href='https://github.com/Josh1794'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <i className='fab fa-github'></i>
+        </a>
+
+        <a
+          className={styles.sociala}
+          aria-label='Stack Overflow Profile'
+          href='https://stackoverflow.com/users/12751027/josh'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <i className='fab fa-stack-overflow'></i>
+        </a>
+      </div>
+    </div>
+  );
+}
