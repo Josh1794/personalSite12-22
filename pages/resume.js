54a3e3e529b644688dd93d6ac29126c31f533add
Joshua Grossman <josh1794@gmail.com>
2023-01-01 17:40:36 -0500

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
   return (
     <div className='bg-white dark:bg-black w-full h-full flex-col flex rounded-lg p-4'>
       <a href={'/'} style={{ top: 20, left: 20, fontSize: 30 }}>
-        &larr;
+        <h1 className='text-black dark:text-white'>&larr;</h1>
       </a>
       <ResumeBox />
     </div>
