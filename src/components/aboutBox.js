5de70e5a040cd9e1a6822960ec5612a2f7b665e4
Joshua Grossman <josh1794@gmail.com>
2023-01-01 17:08:42 -0500

    cont...

--- src/components/aboutBox.js
+++ src/components/aboutBox.js
@@ -5,13 +5,14 @@ import profilePic from '../images/profilepic.jpg';
 export default function AboutBox() {
   return (
     <div className={styles.aboutBox}>
-      <h1 aria-label='about me title' className={styles.titleText}>
+      <h1
+        aria-label='about me title'
+        className='text-2xl text-black dark:text-white flex flex-row items-center'
+      >
         About Me
         <Image
+          className='rounded-full h-11 w-11'
           src={profilePic}
-          height={40}
-          width={40}
-          style={{ borderRadius: 25, marginLeft: 10 }}
           loading={'lazy'}
         />
       </h1>
