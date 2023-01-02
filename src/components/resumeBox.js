5d921a569bc7778cf75f0a2039e48b649f64909d
Joshua Grossman <josh1794@gmail.com>
2023-01-02 09:50:15 -0500

    adjusted resume pdf

--- src/components/resumeBox.js
+++ src/components/resumeBox.js
@@ -2,13 +2,18 @@ import styles from '../../styles/Home.module.scss';
 
 export default function ResumeBox() {
   return (
-    <div className='bg-neutral-300 dark:bg-neutral-800 w-full flex-col flex justify-center items-center h-11/12 w-11/12 rounded-lg p-4 self-center'>
+    <div className='bg-neutral-300 dark:bg-neutral-800 flex-col flex justify-center items-center h-5/6 w-11/12 rounded-lg p-4 self-center'>
       <h1 className='text-2xl text-black dark:text-white flex flex-row items-center self-center'>
         My Resume
       </h1>
+
       <iframe
-        src='https://lavender-ardis-15.tiiny.site'
-        style={{ height: '80vh', width: '80vw', marginBottom: 20 }}
+        src='https://drive.google.com/file/d/1iylhH1h-12YXC--5honTOU6t0bm0uxKV/preview?usp=embed_googleplus'
+        style={{
+          width: '80vw',
+          height: '80vh',
+        }}
+        allowFullScreen
       />
     </div>
   );
