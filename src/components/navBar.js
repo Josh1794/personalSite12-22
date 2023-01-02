b3c44006f2bb457579dd0fb6e343d448b11d534a
Joshua Grossman <josh1794@gmail.com>
2023-01-02 10:03:41 -0500

    fixed zindex

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,7 +3,7 @@ import styles from '../../styles/Home.module.scss';
 
 export default function NavBar() {
   return (
-    <nav className='w-full px-2 bg-white dark:bg-black flex flex-row items-center justify-between h-14 fixed'>
+    <nav className='z-10 w-full px-2 bg-white dark:bg-black flex flex-row items-center justify-between h-14 fixed'>
       {/* NEED TO ADD DARK/LIGHT MODE CONTROL */}
       <h2 className='text-2xl text-orange-400'>Joshua Grossman</h2>
       <Link classname='text-1xl justify-end' href={'/resume'}>
